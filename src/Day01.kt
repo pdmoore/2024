4d7068bded93f9e910af8cee200cb1fe5ee32a9f
Paul Moore <pdmoore@gmail.com>
2024-12-01 19:13:07 -0800

    2024 day01 part 2 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,7 +4,6 @@ import kotlin.math.abs
 
 fun main() {
     fun part1(input: List<String>): Int {
-
         var firstList: List<Int> = ArrayList()
         var secondList: List<Int> = ArrayList()
 
@@ -26,12 +25,30 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var firstList: List<Int> = ArrayList()
+        var secondList: List<Int> = ArrayList()
+
+        for (s in input) {
+            val s1 = s.split(" {3,}".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            firstList += s1[0].toInt()
+            secondList += s1[1].toInt()
+        }
+
+        val secondListAppearance = HashMap<Int, Int>()
+        for (integer in secondList) {
+            secondListAppearance[integer] = secondListAppearance.getOrDefault(integer, 0) + 1
+        }
+
+        return firstList
+            .stream()
+            .mapToInt { integer: Int -> integer * secondListAppearance.getOrDefault(integer, 0) }
+            .sum()
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_example")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
