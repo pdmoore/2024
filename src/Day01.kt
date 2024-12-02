00076edb083605ebc8728d723c6ef454800f03c6
Paul Moore <pdmoore@gmail.com>
2024-12-01 19:11:00 -0800

    2024 day01 - part 1 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,37 @@
+import java.util.*
+import java.util.stream.IntStream
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var firstList: List<Int> = ArrayList()
+        var secondList: List<Int> = ArrayList()
+
+        for (s in input) {
+            val s1 = s.split(" {3,}".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            firstList += s1[0].toInt()
+            secondList += s1[1].toInt()
+        }
+        Collections.sort(firstList)
+        Collections.sort(secondList)
+
+        return IntStream
+            .range(0, firstList.size)
+            .map { i: Int ->
+                abs((firstList[i] - secondList[i]).toDouble())
+                    .toInt()
+            }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_example")
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
