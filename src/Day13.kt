a9cb5d3d3bc6082e5a54823458f7b8c703107a7d
Paul Moore <pdmoore@gmail.com>
2024-12-13 19:06:20 -0800

    2024 day13 solved

---
+++ src/Day13.kt
@@ -0,0 +1,87 @@
+import java.math.BigInteger
+
+fun main() {
+    val testInput = readInput("Day13_example")
+    check(part1(testInput, BigInteger.ZERO) == BigInteger("480"))
+    check(part1(testInput, BigInteger("10000000000000")) == BigInteger("875318608908"))
+
+    val input = readInput("Day13")
+    part1(input, BigInteger.ZERO).println()
+    part1(input, BigInteger("10000000000000")).println()
+}
+
+    class ButtonPushes {
+        var tokenCostBI: BigInteger? = null
+        var aBI: BigInteger? = null
+        var bBI: BigInteger? = null
+
+        constructor(pushA: BigInteger, pushB: BigInteger?) {
+            aBI = pushA
+            bBI = pushB
+
+            tokenCostBI = pushA.multiply(BigInteger.valueOf(3)).add(pushB)
+        }
+    }
+
+
+fun part1(input: List<String>, offset: BigInteger): Any {
+    var tokensNeeded = BigInteger.ZERO
+
+        var i = 0
+        while (i < input.size) {
+            val bp: ButtonPushes = findWinningMovesPart2(input.subList(i, i + 3), offset)
+            tokensNeeded = tokensNeeded.add(bp.tokenCostBI)
+            i += 4
+        }
+
+
+    return tokensNeeded
+}
+
+fun findWinningMovesPart2(input: List<String>, offset: BigInteger): ButtonPushes {
+    val aValues: Array<String> =
+        input.get(0).replace("Button A:", "").trim { it <= ' ' }.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+            .toTypedArray() // a deltas
+    val bValues: Array<String> =
+        input.get(1).replace("Button B:", "").trim { it <= ' ' }.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+            .toTypedArray() // a deltas
+    val targets: Array<String> =
+        input.get(2).replace("Prize:", "").trim { it <= ' ' }.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+            .toTypedArray() // a deltas
+
+    val aXmovement = BigInteger(aValues[0].split("\\+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1])
+    val aYmovement = BigInteger(aValues[1].split("\\+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1])
+
+    val bXmovement = BigInteger(bValues[0].split("\\+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1])
+    val bYmovement = BigInteger(bValues[1].split("\\+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1])
+
+
+    val longX = targets[0].split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1].toLong()
+    val longY = targets[1].split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1].toLong()
+
+
+    val prize0 = offset.add(BigInteger.valueOf(longX))
+    val prize1 = offset.add(BigInteger.valueOf(longY))
+
+    val det = aXmovement.multiply(bYmovement)
+        .subtract(aYmovement.multiply(bXmovement))
+
+    val a = prize0.multiply(bYmovement)
+        .subtract(prize1.multiply(bXmovement))
+        .divide(det)
+
+    val b = prize1.multiply(aXmovement)
+        .subtract(prize0.multiply(aYmovement))
+        .divide(det)
+
+    val checkX = a.multiply(aXmovement)
+        .add(b.multiply(bXmovement))
+    val checkY = a.multiply(aYmovement)
+        .add(b.multiply(bYmovement))
+    if (prize0 == checkX && prize1 == checkY) {
+        return ButtonPushes(a, b)
+    }
+
+    return ButtonPushes(BigInteger.ZERO, BigInteger.ZERO)
+}
+
